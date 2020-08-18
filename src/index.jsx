dd9f1f5928838927344d7a01b56aff37fdaccef2
clickf5 <paninhome@gmail.com>
2020-08-18 11:14:11 +0300

    rename to 'components'

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './component/App';
+import App from './components/App';
 
 render(<App />, document.getElementById('root'));
