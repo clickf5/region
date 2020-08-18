b8cf6cd2b6595fd143c22f81609cabf3b47ed0c3
clickf5 <paninhome@gmail.com>
2020-08-18 17:07:21 +0300

    add class component "Form" with state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from './Form';
 
 const App = () => (
   <>
@@ -11,68 +12,7 @@ const App = () => (
     </div>
     <div className="row">
       <div className="col">
-        <form>
-          <div className="form-row align-items-end">
-            <div className="form-group col-5">
-              <label htmlFor="data">Данные:</label>
-              <select className="form-control" id="data" name="data">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-            </div>
-            <div className="form-group col-1">
-              <div className="btn-group btn-block" role="group" aria-label="Basic example">
-                <button type="button" className="btn btn-success">+</button>
-                <button type="button" className="btn btn-danger">-</button>
-              </div>
-            </div>
-            <div className="form-group col-2">
-              <label htmlFor="salon_id">ID салона:</label>
-              <input type="text" className="form-control" id="salon_id" />
-            </div>
-            <div className="form-group col-2">
-              <label htmlFor="delimeter">Разделитель:</label>
-              <input type="text" className="form-control" id="delimeter" />
-            </div>
-            <div className="form-group col-2">
-              <label htmlFor="sort_from">Сортировка начиная с:</label>
-              <input type="text" className="form-control" id="sort_from" />
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-2">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" id="truncate" value="option1" />
-                <label className="form-check-label" htmlFor="truncate">Добавить TRUNCATE</label>
-              </div>
-            </div>
-            <div className="form-group col-4">
-              <div className="form-check">
-                <input className="form-check-input" type="checkbox" id="truncate" value="option1" />
-                <label className="form-check-label" htmlFor="truncate">Отключить проверку связанных ключей</label>
-              </div>
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-6">
-              <textarea className="form-control" rows="10" />
-            </div>
-            <div className="form-group col-6">
-              <textarea className="form-control" rows="10" />
-            </div>
-          </div>
-          <div className="form-row">
-            <div className="form-group col-6">
-              <button type="button" className="btn btn-success">Копировать</button>
-            </div>
-            <div className="form-group col-6">
-              <button type="button" className="btn btn-success">Копировать</button>
-            </div>
-          </div>
-        </form>
+        <Form />
       </div>
     </div>
   </>
---
+++ src/components/Form.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      regions: [
+        { id: 1, name: 'Тульская область' },
+        { id: 2, name: 'Московская область' },
+      ],
+      form: {
+        region: 'Тульская область',
+        salonId: '1',
+        delimeter: ',',
+        sortFrom: '0',
+      },
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const { form } = this.state;
+    this.setState({ form: { ...form, [name]: value } });
+  }
+
+  renderRegionsSelect() {
+    const { regions, form: { region } } = this.state;
+    const options = regions.map(({ id, name }) => (
+      <option key={id}>{name}</option>
+    ));
+    return (
+      <select onChange={this.handleChange} className="form-control" id="region" value={region} name="region">
+        {options}
+      </select>
+    );
+  }
+
+  render() {
+    const { salonId, delimeter, sortFrom } = this.state.form;
+    return (
+      <form>
+        <div className="form-row align-items-end">
+          <div className="form-group col-5">
+            <label htmlFor="regions">Данные:</label>
+            {this.renderRegionsSelect()}
+          </div>
+          <div className="form-group col-1">
+            <div className="btn-group btn-block" role="group" aria-label="Basic example">
+              <button type="button" className="btn btn-success">+</button>
+              <button type="button" className="btn btn-danger">-</button>
+            </div>
+          </div>
+          <div className="form-group col-2">
+            <label htmlFor="salon-id">ID салона:</label>
+            <input onChange={this.handleChange} type="text" className="form-control" id="salon-id" name="salonId" value={salonId} />
+          </div>
+          <div className="form-group col-2">
+            <label htmlFor="delimeter">Разделитель:</label>
+            <input onChange={this.handleChange} type="text" className="form-control" id="delimeter" name="delimeter" value={delimeter} />
+          </div>
+          <div className="form-group col-2">
+            <label htmlFor="sort_from">Сортировка начиная с:</label>
+            <input onChange={this.handleChange} type="text" className="form-control" id="sort-from" name="sortFrom" value={sortFrom} />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="truncate" value="option1" />
+              <label className="form-check-label" htmlFor="truncate">Добавить TRUNCATE</label>
+            </div>
+          </div>
+          <div className="form-group col-4">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="truncate" value="option1" />
+              <label className="form-check-label" htmlFor="truncate">Отключить проверку связанных ключей</label>
+            </div>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-6">
+            <textarea className="form-control" rows="10" />
+          </div>
+          <div className="form-group col-6">
+            <textarea className="form-control" rows="10" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group col-6">
+            <button type="button" className="btn btn-success">Копировать</button>
+          </div>
+          <div className="form-group col-6">
+            <button type="button" className="btn btn-success">Копировать</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
