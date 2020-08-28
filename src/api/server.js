776074035f98f17bb21df400b6c9a5d37d54cc1a
clickf5 <paninhome@gmail.com>
2020-08-28 16:53:08 +0300

    use mongodb fo all API method

--- src/api/server.js
+++ src/api/server.js
@@ -1,20 +1,18 @@
 import Express from 'express';
 import { ObjectID } from 'mongodb';
-import Region from './Region';
-
-let regions = [
-  new Region('Тульская область', 'Тула,Арсеньевский р-он,Белёвский р-он,Богородицкий р-он,Венёвский р-он,Воловский р-он,Дубенский р-он,Заокский р-он,Каменский р-он,Кимовский р-он,Киреевский р-он,Куркинский р-он,Одоевский р-он,Плавский р-он,Суворовский р-он,Тёпло-Огарёвский р-он,Узловский р-он,Чернский р-он,Щёкинский р-он,Ясногорский р-он,Калужская обл,Липецкая обл,Московская обл,Орловская обл,Рязанская обл'),
-  new Region('Московская область', 'Москва,Волоколамский р-он,Воскресенский р-он,Дмитровский р-он,Клинский р-он,Ленинский р-он,Лотошинский р-он,Можайский р-он,Ногинский р-он,Одинцовский р-он,Орехово-Зуевский р-он,Пушкинский р-он,Раменский р-он,Сергиево-Посадский р-он,Серпуховский р-он,Солнечногорский р-он,Талдомский р-он,Щёлковский р-он,Владимирская обл,Калужская обл,Рязанская обл,Смоленская обл,Тверская обл,Тульская обл'),
-];
 
 const server = new Express();
 
 server.use(Express.json());
 
 export default (database) => {
+  // GET /regions
   server.get('/regions', async (req, res) => {
     try {
-      const result = await database.collection('regions').find().toArray();
+      const result = await database
+        .collection('regions')
+        .find()
+        .toArray();
       res.json({ success: true, payload: result });
     } catch (e) {
       res.status(422);
@@ -22,9 +20,13 @@ export default (database) => {
     }
   });
 
+  // GET /region/:id
   server.get('/region/:id', async (req, res) => {
     try {
-      const result = await database.collection('regions').findOne(new ObjectID(req.params.id));
+      const result = await database
+        .collection('regions')
+        .findOne({ _id: new ObjectID(req.params.id) });
+        // .findOne(new ObjectID(req.params.id));
       res.json({ success: true, payload: result });
     } catch (e) {
       res.status(422);
@@ -32,7 +34,8 @@ export default (database) => {
     }
   });
 
-  server.post('/region', (req, res) => {
+  // POST /region
+  server.post('/region', async (req, res) => {
     const { name, districts } = req.body;
 
     const errors = {};
@@ -50,39 +53,41 @@ export default (database) => {
       res.json({ success: false, errors });
     }
 
-    const region = new Region(name, districts);
-    regions.push(region);
-    res.status(201);
-    res.json({ success: true, payload: region });
-  });
-
-  server.delete('/region/:id', (req, res) => {
-    const region = regions.find(({ id }) => id.toString() === req.params.id);
-
-    const errors = {};
-
-    if (region === undefined) {
-      errors.message = `Not found region with id: ${req.params.id}`;
-    }
-
-    if (Object.keys(errors).length > 0) {
+    try {
+      const { ops } = await database
+        .collection('regions')
+        .insertOne({ name, districts });
+      res.status(201);
+      res.json({ success: true, payload: ops[0] });
+    } catch (e) {
       res.status(422);
-      res.json({ success: false, errors });
+      res.json({ success: false, errors: { message: e.message } });
     }
-
-    regions = regions.filter(({ id }) => id !== region.id);
-    res.json({ success: true });
   });
 
-  server.put('/region/:id', (req, res) => {
+  // DELETE /region/:id
+  server.delete('/region/:id', async (req, res) => {
+    try {
+      const { value } = await database
+        .collection('regions')
+        .findOneAndDelete({ _id: new ObjectID(req.params.id) });
+
+      if (value === null) {
+        throw new Error(`Document with _id: ${req.params.id} not found`);
+      }
+
+      res.json({ success: true, payload: value });
+    } catch (e) {
+      res.status(422);
+      res.json({ success: false, errors: { message: e.message } });
+    }
+  });
+
+  // PUT /region/:id
+  server.put('/region/:id', async (req, res) => {
     const { name, districts } = req.body;
-    const region = regions.find(({ id }) => id.toString() === req.params.id);
 
     const errors = {};
-
-    if (region === undefined) {
-      errors.message = `Not found region with id: ${req.params.id}`;
-    }
 
     if (name === undefined || name === '') {
       errors.name = "name cant't be blank";
@@ -97,9 +102,21 @@ export default (database) => {
       res.json({ success: false, errors });
     }
 
-    const updated = { ...region, name, districts };
-    regions = regions.map((r) => ((r.id === region.id) ? updated : r));
-    res.json({ success: true, payload: updated });
+    try {
+      const update = { $set: { name, districts } };
+      const { value } = await database
+        .collection('regions')
+        .findOneAndUpdate({ _id: new ObjectID(req.params.id) }, update);
+
+      if (value === null) {
+        throw new Error(`Document with _id: ${req.params.id} not found`);
+      }
+
+      res.json({ success: true, payload: value });
+    } catch (e) {
+      res.status(422);
+      res.json({ success: false, errors: { message: e.message } });
+    }
   });
 
   return server;
