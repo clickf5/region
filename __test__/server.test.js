f71ee41918173e320244adc9487423fdd2ee4571
clickf5 <paninhome@gmail.com>
2020-08-30 14:41:49 +0300

    fix test

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -12,15 +12,15 @@ const options = {
   useNewUrlParser: true,
 };
 
-let uri;
 let mongoClient;
+let app;
 
 describe('requests', () => {
   beforeAll(async () => {
     expect.extend(matchers);
 
     const mongoServer = new MongoMemoryServer();
-    uri = await mongoServer.getUri();
+    const uri = await mongoServer.getUri();
 
     mongoClient = new MongoClient(uri, options);
     await mongoClient.connect();
@@ -31,15 +31,17 @@ describe('requests', () => {
         { name: 'Тульская область', districts: 'Тула' },
         { name: 'Московкасая область', districts: 'Москва' },
       ]);
+
+    app = server(uri, dbName);
   });
 
-  afterEach(() => {
+  afterAll(() => {
     mongoClient.close();
   });
 
   it('GET /regions', async () => {
     try {
-      const result = await request(server(uri, dbName))
+      const result = await request(app)
         .get('/regions');
       expect(result).toHaveHTTPStatus(200);
     } catch (e) {
@@ -47,47 +49,62 @@ describe('requests', () => {
     }
   });
 
-  // it('GET /region/:id', async () => {
-  //   const res = await request(server(database))
-  //     .get('/region/1');
-  //   expect(res).toHaveHTTPStatus(200);
+  it('GET /region/:id', async () => {
+    const { body } = await request(app)
+      .post('/region')
+      .send({ name: 'Воронежская область', districts: 'Воронеж' });
+    const { _id: id } = body.payload;
 
-  //   const fail = await request(server(database))
-  //     .get('/region/3');
-  //   expect(fail).toHaveHTTPStatus(422);
-  // });
+    const result = await request(app)
+      .get(`/region/${id}`);
+    expect(result).toHaveHTTPStatus(200);
 
-  // it('POST /region', async () => {
-  //   const res = await request(server(database))
-  //     .post('/region')
-  //     .send({ name: 'Воронежская область', districts: 'Воронеж' });
-  //   expect(res).toHaveHTTPStatus(201);
+    const fail = await request(app)
+      .get('/region/3');
+    expect(fail).toHaveHTTPStatus(422);
+  });
 
-  //   const fail = await request(server(database))
-  //     .post('/region')
-  //     .send({ name: '', districts: '' });
-  //   expect(fail).toHaveHTTPStatus(422);
-  // });
+  it('POST /region', async () => {
+    const result = await request(app)
+      .post('/region')
+      .send({ name: 'Воронежская область', districts: 'Воронеж' });
+    expect(result).toHaveHTTPStatus(201);
 
-  // it('PUT /region/:id', async () => {
-  //   const res = await request(server(database))
-  //     .put('/region/1')
-  //     .send({ name: 'Липетцкая область', districts: 'Липетцк' });
-  //   expect(res).toHaveHTTPStatus(200);
+    const fail = await request(app)
+      .post('/region')
+      .send({ name: '', districts: '' });
+    expect(fail).toHaveHTTPStatus(422);
+  });
 
-  //   const fail = await request(server(database))
-  //     .put('/region/1')
-  //     .send({ name: '', districts: '' });
-  //   expect(fail).toHaveHTTPStatus(422);
-  // });
+  it('PUT /region/:id', async () => {
+    const { body } = await request(app)
+      .post('/region')
+      .send({ name: 'Воронежская область', districts: 'Воронеж' });
+    const { _id: id } = body.payload;
 
-  // it('DELETE /region/:id', async () => {
-  //   const res = await request(server(database))
-  //     .delete('/region/1');
-  //   expect(res).toHaveHTTPStatus(200);
+    const result = await request(app)
+      .put(`/region/${id}`)
+      .send({ name: 'Липетцкая область', districts: 'Липетцк' });
+    expect(result).toHaveHTTPStatus(200);
 
-  //   const fail = await request(server(database))
-  //     .get('/region/1');
-  //   expect(fail).toHaveHTTPStatus(422);
-  // });
+    const fail = await request(app)
+      .put(`/region/${id}`)
+      .send({ name: '', districts: '' });
+    expect(fail).toHaveHTTPStatus(422);
+  });
+
+  it('DELETE /region/:id', async () => {
+    const { body } = await request(app)
+      .post('/region')
+      .send({ name: 'Воронежская область', districts: 'Воронеж' });
+    const { _id: id } = body.payload;
+
+    const result = await request(app)
+      .delete(`/region/${id}`);
+    expect(result).toHaveHTTPStatus(200);
+
+    const fail = await request(app)
+      .get('/region/1');
+    expect(fail).toHaveHTTPStatus(422);
+  });
 });
